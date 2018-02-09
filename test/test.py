5f09a9232d56c5921b0c4b7252ffa8037b78c461
jirenhaung <1072425143@qq.com>
2018-02-09 17:15:17 +0800

    增加两个

---
+++ test/test.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+if True:
+    print "true"
+else:
+    print "false"
+print "ad"
+x = "a"
+y = "b"
+print x
+print y
+print x,y
+print x,
+print y,
+raw_input("按下 enter 键退出，其他任意键显示...\n")  # \n 实现换行。一旦用户按下 enter(回车) 键退出，其它键显示。
---
+++ test/try.py
@@ -0,0 +1,5 @@
+#coding=utf-8
+import sys
+
+print '参数个数为:', len(sys.argv), '个参数。'
+print '参数列表:', str(sys.argv)
